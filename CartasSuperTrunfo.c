95981875d70be1f12f0cf254e4f85c5e37e98444
Tachadd <wendergb222@gmail.com>
2025-10-06 17:48:14 +0000

    Segunda etapa do desafio feita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,17 +11,19 @@
 
   int main() {
   
+    
     printf("CARTA 1\n");
+    
     
     char Estado [20], nome[20];
     int população, codigo, pontos;
-    float área, PIB;
+    float área, PIB, Densidade, capita;
 
     printf("Qual o Nome do Estado?  \n");
     scanf("%s", &Estado);
 
     printf("Qual o código da sua carta? \n");
-    scanf("%d", &codigo);
+    scanf("%s", &codigo);
 
     printf("Qual o nome da cidade? \n");
     scanf("%s", &nome);
@@ -37,12 +39,18 @@
 
     printf("Qual o PIB? \n");
     scanf("%f", &PIB);
+
+    //segunda parte do desafio
+    Densidade = (float) população / área;
+    capita = (float) PIB / população;
   
+    
     printf("CARTA 2\n");
+    
     
     char Estado2 [20], nome2[20];
     int população2, codigo2, pontos2;
-    float área2, PIB2;
+    float área2, PIB2, densidade2, capita2;
 
     printf("Qual o Nome do Estado?  \n");
     scanf("%s", &Estado2);
@@ -65,7 +73,14 @@
     printf("Qual o PIB? \n");
     scanf("%f", &PIB2);
     
+    //segunda parte do desafio
+    densidade2 = (float) população / área;
+    capita2 = (float) PIB / população;
+
+    
     //CARTA 1
+    
+    
     printf("CARTA 1\n");
     
     
@@ -83,23 +98,33 @@
     
     printf("PIB: %3.f\n", PIB);
     
+    printf("Densidade populacional: %.2f km²\n", Densidade);
+
+    printf("PIB per capita: %.2f reais\n", capita);
+    
+    
     //CARTA 2
+    
+    
     printf("CARTA 2\n");
     
+    printf("ESTADO: %s\n", Estado2);
     
-    printf("ESTADO: %s\n", Estado);
+    printf("CÓDIGO DA CARTA: %d\n", codigo2);
     
-    printf("CÓDIGO DA CARTA: %d\n", codigo);
+    printf("CIDADE: %s\n", nome2);
     
-    printf("CIDADE: %s\n", nome);
+    printf("NÚMERO DE HABITANTES: %d\n", população2);
     
-    printf("NÚMERO DE HABITANTES: %d\n", população);
+    printf("NÚMERO DE PONTOS TURÍSTICOS: %d\n", pontos2);
     
-    printf("NÚMERO DE PONTOS TURÍSTICOS: %d\n", pontos);
+    printf("TAMANHO EM KM2: %3.f\n", área2 );
     
-    printf("TAMANHO EM KM2: %3.f\n", área );
-    
-    printf("PIB: %3.f\n", PIB);
+    printf("PIB: %3.f\n", PIB2);
+
+    printf("Densidade populacional: %.2f km²\n", densidade2);
+
+    printf("PIB per capita: %.2f reais\n", capita2);
 
     return 0 ;
   }
