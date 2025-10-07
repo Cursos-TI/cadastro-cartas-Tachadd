e8d2826105ea30ad110588cc555f9c067ce0d788
Tachadd <wendergb222@gmail.com>
2025-10-07 23:42:13 +0000

    DESAFIO FINALIZADO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,7 +17,7 @@
     
     char Estado[20], nome[20];
     int população, codigo, pontos;
-    float área, PIB, Densidade, capita;
+    float área, PIB, Densidade, capita, superpoder;
 
     printf("Qual o Nome do Estado?  \n");
     scanf("%s", &Estado);
@@ -40,17 +40,20 @@
     printf("Qual o PIB? \n");
     scanf("%f", &PIB);
 
-    //segunda parte do desafio
+    //SEGUNDA PARTE DO DESAFIO
+    
     Densidade = (float) população / área;
     capita = (float) PIB / população;
-  
+    
+    //TERCEIRA PARTE DO DESAFIO
+    superpoder = (float) população + área + PIB + pontos + capita + (1 / Densidade);
     
     printf("CARTA 2\n");
     
     
     char Estado2 [20], nome2[20];
     int população2, codigo2, pontos2;
-    float área2, PIB2, densidade2, capita2;
+    float área2, PIB2, densidade2, capita2, superpoder2;
 
     printf("Qual o Nome do Estado?  \n");
     scanf("%s", &Estado2);
@@ -73,17 +76,19 @@
     printf("Qual o PIB? \n");
     scanf("%f", &PIB2);
     
-    //segunda parte do desafio
-    densidade2 = (float) população / área;
-    capita2 = (float) PIB / população;
-
+    //SEGUNDA PARTE DO DESAFIO
+    densidade2 = (float) população2 / área2;
+    capita2 = (float) PIB2 / população2;
+    
+    //TERCEIRA PARTE DO DESAFIO
+    superpoder2 = (float) população2 + área2 + PIB2 + pontos2 + capita2 + (1 / densidade2);
     
     //CARTA 1
     
     
     printf("CARTA 1\n");
     
-    
+
     printf("ESTADO: %s\n", Estado);
     
     printf("CÓDIGO DA CARTA: %d\n", codigo);
@@ -102,7 +107,8 @@
 
     printf("PIB per capita: %.2f reais\n", capita);
     
-    
+    printf (" Super Poder: %2.f\n", superpoder);
+
     //CARTA 2
     
     
@@ -125,6 +131,19 @@
     printf("Densidade populacional: %.2f km²\n", densidade2);
 
     printf("PIB per capita: %.2f reais\n", capita2);
+    
+    printf ("Super Poder: %2.f\n", superpoder2);
+
+
+    printf("    ***comparação das cartas***     \n");
+
+    printf("população: carta1 venceu %d\n", população > população2);
+    printf("área: carta1 venceu %d\n", área > área2);
+    printf("PIB: carta1 venceu %d\n", PIB > PIB2);
+    printf("Pontos turísticos: carta1 venceu %d\n", pontos > pontos2);
+    printf("Densidade populacional: carta1 venceu %d\n", Densidade < densidade2); //menor vence
+    printf("PIB per Capita: carta1 venceu %d\n", capita > capita2);
+    printf("Super Poder: carta1 venceu %d\n", superpoder > superpoder2);
 
     return 0 ;
   }
